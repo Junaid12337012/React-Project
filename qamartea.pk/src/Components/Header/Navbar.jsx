29c191954b2e1dc5370066deb6a5a0c5a0ebdcd9
junaid khan <146834518+Junaid12337012@users.noreply.github.com>
2024-07-24 08:08:49 +0000

    commit

--- qamartea.pk/src/Components/Header/Navbar.jsx
+++ qamartea.pk/src/Components/Header/Navbar.jsx
@@ -1,6 +1,4 @@
-// src/Navbar.js
-
-import './Navbar.css'; // Import custom CSS for additional styles
+import './Navbar.css';
 
 import React, { useState } from 'react';
 
@@ -17,7 +15,7 @@ const Navbar = () => {
         <div className="text-black text-2xl font-bold">
           <a href="#">Brand</a>
         </div>
-        <div className="block lg:hidden">
+        <div className="block md:hidden">
           <button 
             onClick={toggleMenu} 
             className={`text-black flex flex-col focus:outline-none transition-transform duration-300 ease-in-out ${isOpen ? 'rotate-90' : ''}`}
@@ -29,8 +27,18 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
-        <div id="navbar-menu" className={`w-full lg:flex lg:items-start lg:w-auto transition-all duration-300 ease-in-out ${isOpen ? 'block' : 'hidden'}`}>
+        {/*  for mobile and short tablet */}
+        <div id="navbar-menu" className={`lg:flex  lg:w-auto ${isOpen ? 'block' : 'hidden'} transition-all duration-300 ease-in-out`}>
           <div className="lg:flex lg:space-x-4">
+            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Home</a>
+            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">About</a>
+            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Services</a>
+            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Contact</a>
+          </div>
+         </div>
+         {/*for  laptop and pc */}
+         <div className={`lg:flex lg:w-auto transition-all duration-300 ease-in-out`} >
+          <div className=" lg:space-x-4">
             <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Home</a>
             <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">About</a>
             <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Services</a>

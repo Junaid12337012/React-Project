650ab580e5f3cadf087cb49e6e3f66ee3966f55d
junaid khan <junaidmushtaq988@gmail.com>
2024-07-24 14:15:59 +0500

    Make responsive Navbar

--- qamartea.pk/src/Components/Header/Navbar.jsx
+++ qamartea.pk/src/Components/Header/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
           <a href="#">Brand</a>
         </div>
         <div className="block md:hidden">
-          <button 
-            onClick={toggleMenu} 
+          <button
+            onClick={toggleMenu}
             className={`text-black flex flex-col focus:outline-none transition-transform duration-300 ease-in-out ${isOpen ? 'rotate-90' : ''}`}
             aria-expanded={isOpen}
             aria-controls="navbar-menu"
@@ -27,24 +27,33 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
-        {/*  for mobile and short tablet */}
-        <div id="navbar-menu" className={`lg:flex  lg:w-auto ${isOpen ? 'block' : 'hidden'} transition-all duration-300 ease-in-out`}>
-          <div className="lg:flex lg:space-x-4">
-            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Home</a>
-            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">About</a>
-            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Services</a>
-            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Contact</a>
+        {/* Mobile menu */}
+        <div id="navbar-menu" className={`block ${isOpen ? 'block' : 'hidden'} md:hidden lg:hidden transition-all duration-300 ease-in-out`}>
+          <div className="flex flex-col space-y-2">
+            <a href="#" className="block text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Home</a>
+            <a href="#" className="block text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">About</a>
+            <a href="#" className="block text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Services</a>
+            <a href="#" className="block text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Contact</a>
           </div>
-         </div>
-         {/*for  laptop and pc */}
-         <div className={`lg:flex lg:w-auto transition-all duration-300 ease-in-out`} >
-          <div className=" lg:space-x-4">
-            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Home</a>
-            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">About</a>
-            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Services</a>
-            <a href="#" className="block lg:inline-block text-black hover:bg-yellow-700 px-3 py-2 rounded mt-1 lg:mt-0 transition-colors duration-300">Contact</a>
+        </div>
+        {/* Tablet menu */}
+        <div className="hidden md:block lg:hidden">
+          <div className="flex space-x-4">
+            <a href="#" className="text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Home</a>
+            <a href="#" className="text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">About</a>
+            <a href="#" className="text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Services</a>
+            <a href="#" className="text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Contact</a>
           </div>
-         </div>
+        </div>
+        {/* Desktop menu */}
+        <div className="hidden lg:block">
+          <div className="flex space-x-4">
+            <a href="#" className="text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Home</a>
+            <a href="#" className="text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">About</a>
+            <a href="#" className="text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Services</a>
+            <a href="#" className="text-black hover:bg-yellow-700 px-3 py-2 rounded transition-colors duration-300">Contact</a>
+          </div>
+        </div>
       </div>
     </nav>
   );

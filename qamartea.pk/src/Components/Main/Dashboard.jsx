7721e33d5b2f584a410533134ac438f8a8749b33
junaid khan <146834518+Junaid12337012@users.noreply.github.com>
2024-07-25 14:04:24 +0000

    DASHBOARD CHANGES

--- qamartea.pk/src/Components/Main/Dashboard.jsx
+++ qamartea.pk/src/Components/Main/Dashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const Dashboard = () => {
+  const imageUrl = 'https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg';
+
   return (
-    <div>Dashboard</div>
+    <div className='container flex'>
+      <div>
+        <h1 className='font-bold'>logo</h1>
+        <h1>hello world</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum assumenda commodi inventore ullam, fuga a iure odit minima, tempore impedit porro labore maxime voluptas officia? Possimus ad error neque at.</p>
+      </div>
+      <div>
+        <img src={imageUrl} alt="View of street from a glass window" />
+      </div>
+    </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
